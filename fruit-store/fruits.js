316851f620b0a2629e2c43c3acc9f363c3d115de
Eduardo Rizzo de Albuquerque <erizzoalbuquerque@gmail.com>
2019-06-09 21:30:58 -0300

    criando server, retornando json de futas

---
+++ fruit-store/fruits.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+      "name": "Berries",
+      "price": 23.54,
+      "image": "/assets/images/berrie.jpg",
+      "description": "The bestest fruit known to man. Sweet yet sour but beautiful"
+    },
+    {
+      "name": "Orange",
+      "price": 10.33,
+      "image": "/assets/images/orange.jpg",
+      "description": "Succulent and watery, you'll never run out of water"
+    },
+    {
+      "name": "Lemons",
+      "price": 12.13,
+      "image": "/assets/images/lemon.jpg",
+      "description": "Sour but important for revitalization"
+    },
+    {
+      "name": "Bananas",
+      "price": 10.33,
+      "image": "/assets/images/banana.jpg",
+      "description": "An every day fruit, can be served with every dish"
+    },
+    {
+      "name": "Apples",
+      "price": 10.33,
+      "image": "/assets/images/apple-item.jpg",
+      "description": "Sliced and served with your salad. Served as snacks midway through the day"
+    },
+    {
+      "name": "Sharifa",
+      "price": 10.33,
+      "image": "/assets/images/unknown.jpg",
+      "description": "A great fruit, also known as custard apple"
+    }
+  ]
